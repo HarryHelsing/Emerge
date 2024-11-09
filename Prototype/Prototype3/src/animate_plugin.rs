0c524f47337c9b446f8eaa6e2a096f2b9a023f38
HarryHelsing <harryhelsing@outlook.com>
2024-11-09 01:15:43 +0000

    Animation finally working!

--- Prototype/Prototype3/src/animate_plugin.rs
+++ Prototype/Prototype3/src/animate_plugin.rs
@@ -3,11 +3,6 @@ use crate::turn_plugin::GlobalMoveEvent;
 use crate::turn_plugin::GlobalAnimateEvent;//merge into 1 crate
 use rand::Rng;
 
-/*ToDo
- * fn to update atlas index based upon state info then trigger event if needed
- * have fn read from resource, stepping through vec to find animation data
- */
-
 pub struct AnimatePlugin;
 
 #[derive(PartialEq)]
@@ -21,7 +16,7 @@ last_frame: usize,
 }
 
 struct AnimationType {
-animation_type: usize,//two nested fields names animation type! fix it
+anim_type: usize,//two nested fields names animation type! fix it
 states: [FrameData; 4],
 }
 
@@ -52,7 +47,7 @@ impl Plugin for AnimatePlugin {
         app.insert_resource(FourStateAnimation {
             animation_type: vec![
                 AnimationType {
-                animation_type: 1,
+                anim_type: 1,
                 states: [
                     FrameData {first_frame: 15, last_frame: 15},
                     FrameData {first_frame: 14, last_frame: 13},
@@ -60,7 +55,7 @@ impl Plugin for AnimatePlugin {
                     FrameData {first_frame: 11, last_frame: 10},
                 ]},
                 AnimationType {
-                animation_type: 2,
+                anim_type: 2,
                 states: [
                     FrameData {first_frame: 20, last_frame: 20},
                     FrameData {first_frame: 19, last_frame: 17},
@@ -113,40 +108,45 @@ let mut keep_going = true;
 //Update entity after state change
 if AnimateOpenClose.just_changed_state {
 keep_going = false;
-AnimateOpenClose.just_changed_state = false;
 
-let animation_vec = animation_data.animation_type;
+let animation_vec = &animation_data.animation_type;
 let target = AnimateOpenClose.animation_type;
-for (animation_type, states) in animation_vec.iter().enumerate()
+for AnimationType in animation_vec.iter()
 {
-    if animation_type == target {
+    if AnimationType.anim_type == target {
         match AnimateOpenClose.animation_states {
     OpenCloseStates::Open => {
-        AnimateOpenClose.first_frame = states[0].first_frame;
-        AnimateOpenClose.last_frame = animation_data.states[0].last_frame;
+        AnimateOpenClose.first_frame = AnimationType.states[0].first_frame;
+        AnimateOpenClose.last_frame = AnimationType.states[0].last_frame;
     }
     OpenCloseStates::Closing => {
-        AnimateOpenClose.first_frame = animation_data.states[1].first_frame;
-        AnimateOpenClose.last_frame = animation_data.states[1].last_frame;
+        AnimateOpenClose.first_frame = AnimationType.states[1].first_frame;
+        AnimateOpenClose.last_frame = AnimationType.states[1].last_frame;
     }
     OpenCloseStates::Closed => {
-        AnimateOpenClose.first_frame = animation_data.states[2].first_frame;
-        AnimateOpenClose.last_frame = animation_data.states[2].last_frame;
+        AnimateOpenClose.first_frame = AnimationType.states[2].first_frame;
+        AnimateOpenClose.last_frame = AnimationType.states[2].last_frame;
     }
     OpenCloseStates::Opening => {
-        AnimateOpenClose.first_frame = animation_data.states[3].first_frame;
-        AnimateOpenClose.last_frame = animation_data.states[3].last_frame;
+        AnimateOpenClose.first_frame = AnimationType.states[3].first_frame;
+        AnimateOpenClose.last_frame = AnimationType.states[3].last_frame;
     }
         }
+TextureAtlas.index = AnimateOpenClose.first_frame;
     }
 
 }
-//create match statement with enum to define which FrameData
-//access animation type to get correct struct from vec
-//access resource, set fields: first_frame, last_frame
-//then apply logic based off of frames to define animateOC fields
+if AnimateOpenClose.first_frame == AnimateOpenClose.last_frame {
+    AnimateOpenClose.no_movement = true } else {
+    AnimateOpenClose.no_movement = false }
 
-AnimateOpenClose.no_movement = false;//temporary, delete later
+if AnimateOpenClose.first_frame > AnimateOpenClose.last_frame {
+    AnimateOpenClose.reverse_animate = true } else {
+    AnimateOpenClose.reverse_animate = false }
+
+AnimateOpenClose.just_changed_state = false;
+
+//then apply logic based off of frames to define animateOC fields
     }
 
 //Iterate through animation
