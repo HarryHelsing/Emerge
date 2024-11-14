041827248a5253c149a3ddaa22cad2ef7261ac5d
HarryHelsing <harryhelsing@outlook.com>
2024-11-14 00:43:54 +0000

    Buggy movement system, needs fixing but principle is there

--- Prototype/Prototype3/src/creature_plugin.rs
+++ Prototype/Prototype3/src/creature_plugin.rs
@@ -40,14 +40,44 @@ on_grid: OnGrid,
 }
 }
 fn move_creature(//creatures could have just moved comp?
-mut creature_query: Query<(&mut Location, &mut RequestLocation, &mut DirectionFacing), With<Creature>>,
-mut player_query: Query<&mut Location, With<Player>>,
+mut creature_query: Query<(&mut Location, &mut RequestLocation, &mut DirectionFacing), (With<Creature>, Without<Player>)>,
+mut player_query: Query<&Location, (With<Player>, Without<Creature>)>,
 mut obstacle_query: Query<&ObstacleLocation>,
 mut move_event: EventReader<GlobalMoveEvent>,
     ) {
 for _event in move_event.read() {
-for (mut location, mut request_location, mut direction_facing) in &mut creature_query {
+    let mut player_x = 0.0;
+    let mut player_y = 0.0;
+    let mut creature_x = 0.0;
+    let mut creature_y = 0.0;
+    let mut blocked = true;
+        for location in &player_query {
+    player_x = location.grid_x;
+    player_y = location.grid_y;
+        }
 
+    for (mut location, mut request_location, mut direction_facing) in &mut creature_query {
+    creature_x = location.grid_x;
+    creature_y = location.grid_y;
+        let difference_x = (player_x - creature_x).abs();
+        let difference_y = (player_y - creature_y).abs();
+        let where_x = creature_x - player_x;
+        let where_y = creature_y - player_y;
+        if difference_x >= difference_y {
+            if where_x > 0.0 {request_location.grid_x = request_location.grid_x - 1.0}
+            else {request_location.grid_x = request_location.grid_x + 1.0}
+        } else {
+if where_y > 0.0 {request_location.grid_y = request_location.grid_y - 1.0}
+            else {request_location.grid_y = request_location.grid_y + 1.0}
+        }
+        for obstacle_location in &obstacle_query {
+     if  request_location.grid_x == obstacle_location.grid_x && request_location.grid_y ==  obstacle_location.grid_y { blocked = false; } 
+        }
+     if !blocked {location.grid_x = request_location.grid_x;
+         location.grid_y = request_location.grid_y;}
 }
+        //if x is same or bigger, move in x axis
+//difference between creature x and player x, c x and c y, then see which is bigger. then go in the
+//direction of the x or y value
 }
 }
